8736375f28054e34ef3a76c1eaa08c59569e4330
ChristopherNewby <cnewbyhst@gmail.com>
2016-11-07 14:53:11 -0500

    message

---
+++ something.js
@@ -0,0 +1,76 @@
+/*document.write('inside my js file');
+
+document.write('<br/>');
+
+var numberOfThings = 5;
+document.write(numberOfThings);
+document.write('<br/>');
+document.write(typeof numberOfThings);
+
+document.write('<br/>');
+
+numberOfThings = 'five';
+document.write(numberOfThings);
+document.write('<br/>');
+document.write(typeof numberOfThings);
+
+var elem = document.getElementById('demo');
+elem.innerHTML = 'some new text';
+
+
+var numDogs = 2;
+var numCats = 2;
+
+var numAnimals = numDogs + numCats;
+var elem = document.getElementById('demo2');
+elem.innerHTML = 'I have ' + numAnimals + ' animals at home';
+
+var cat1Name = 'Tiger'
+var cat2Name = 'Blacky'
+var dog1Name = 'Dori'
+var dog2Name = 'Kronwall'
+
+var elem2 = document.getElementById('demo3');
+elem2.innerHTML = 'The pets names are ' + cat1Name + ', '+ cat2Name + ', '+ dog1Name + ', and '+ dog2Name + '.'
+
+
+
+document.write(x);
+var x = 5;
+
+myFunction();
+
+function myFunction(){
+	var elem2 = document.getElementById('demo3');
+	elem2.innerHTML = 'inside my function';
+}
+*/
+
+var hello = 'Hello World'
+var year = 1942;
+var dogAtHome = true;
+
+var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+
+var amount = 5.45;
+var letter = 'x';
+
+var elem2 = document.getElementById('demo3');
+
+elem2.innerHTML = document.write(hello);	
+document.write('<br/>');
+document.write('Colmbus sailed the world in ' + year);
+document.write('<br/>');
+document.write('it is ' + dogAtHome + ' that I have a dog at home.');
+document.write('<br/>');
+document.write('Today is ' + month + "/" + day + "/" + year);
+document.write('<br/>');
+document.write('I spent &#36;' + amount + ' on a cheeseburger.');
+document.write('<br/>');
+document.write(letter + ' marks the spot!');
+
+var games = ['Destiny','Diablo','Titanfall','Spyro'];
+
